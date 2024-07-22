83b3c7f0aa415aae1c36a22af75f9c58dc80567f
ysamwel <yvonneatieno2000@gmail.com>
2024-07-22 19:52:43 +0300

    0-constant.js

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+      node: true
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended"
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 12,
+      sourceType: "module"
+    },
+    plugins: [
+      "react",
+      "@typescript-eslint"
+    ],
+    rules: {
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"],
+      "no-console": "off"
+    }
+  };
+  
