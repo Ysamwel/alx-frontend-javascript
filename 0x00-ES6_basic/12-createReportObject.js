53baf52ff32a367a4a2676ce3996a5edff8e4698
BarakaCaleb <calebbaraka79@gmail.com>
2024-07-24 00:48:03 +0300

    changes

--- 0x00-ES6_basic/12-createReportObject.js
+++
@@ -1,9 +0,0 @@
-export default function createReportObject(employeesList) {
-    const myObj = {
-      allEmployees: { ...employeesList },
-      getNumberOfDepartments(employeesList) {
-        return Object.keys(employeesList).length;
-      },
-    };
-    return myObj;
-  }
