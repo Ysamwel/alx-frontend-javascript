c34ab20202911dff5455adb0945870e61ae09756
ysamwel <yvonneatieno2000@gmail.com>
2024-07-24 18:11:36 +0300

    more iterators

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
+// Collect all employee names in an array
   const employees = [];
 
   for (const employee of reportWithIterator) {
     employees.push(employee);
   }
 
+  // Join all names with ' | ' separator
   return employees.join(' | ');
 }
