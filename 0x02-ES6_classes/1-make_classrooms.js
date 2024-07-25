949c5ff81bc95ee875a37e2d94eaa315d13dc987
ysamwel <yvonneatieno2000@gmail.com>
2024-07-25 21:57:10 +0300

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,12 +1,12 @@
-import ClassRoom from "./0-classroom";
+// Import the ClassRoom class
+import ClassRoom from './0-classroom';
 
+// Define and export the initializeRooms function
 function initializeRooms() {
-    const rooms = [
-        new ClassRoom =(19),
-        new ClassRoom =(20),
-        new ClassRoom =(34)
-    ];
-    return rooms;
+  const object1 = new ClassRoom(19);
+  const object2 = new ClassRoom(20);
+  const object3 = new ClassRoom(34);
+  const list = [object1, object2, object3];
+  return list;
 }
-
-export {initializeRooms};
+export default initializeRooms;
