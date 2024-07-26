90a87d6d8bb44d3eef59f41dd67befb928d7a304
ysamwel <yvonneatieno2000@gmail.com>
2024-07-26 16:07:53 +0300

    sorting

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,6 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
   const employeeNames = [];
 
+  // Iterate through the iterator object
   for (const employee of reportWithIterator) {
     employeeNames.push(employee);
   }
