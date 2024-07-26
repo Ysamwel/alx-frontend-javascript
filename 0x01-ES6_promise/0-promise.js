f9ded35eb6fb80b541d6414b94722e2a106d6a4e
ysamwel <yvonneatieno2000@gmail.com>
2024-07-26 20:18:24 +0300

    promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,18 +1,3 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      // Simulating asynchronous operation
-      setTimeout(() => {
-        const success = true; // Change to false to test rejection
-  
-        if (success) {
-          resolve('API response data');
-        } else {
-          reject(new Error('API request failed'));
-        }
-      }, 1000); // 1-second delay to simulate API call
-    });
-  }
-  
-  // Export the function as the default export
-  export default getResponseFromAPI;
-  
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
+}
