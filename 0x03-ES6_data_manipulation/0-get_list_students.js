3395e7ebf6e1326c16a439c0c52ba931a6fd34e6
ysamwel <yvonneatieno2000@gmail.com>
2024-07-30 03:00:08 +0300

    data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,23 @@
+function getListStudents() {
+  const students = [
+    {
+      id: 1,
+      firstName: 'Guillaume',
+      location: 'San Francisco',
+    },
+    {
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
+    },
+    {
+      id: 5,
+      firstName: 'Serena',
+      location: 'San Francisco',
+    },
+
+  ];
+  return students;
+}
+const studentsList = getListStudents();
+console.log(studentsList);
