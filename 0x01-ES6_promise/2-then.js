db039cb5043bf3b69371f905f94e9b944ee3b42c
ysamwel <yvonneatieno2000@gmail.com>
2024-10-17 20:58:20 +0300

    promises

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-console */
 export default function handleResponseFromAPI(promise) {
     return promise
       .then(() => ({ status: 200, body: 'success' }))
-      .catch(() => new Error())
-      .finally(() => console.log('Got a response from the API'));
+      .catch(() => Error())
+      .finally(() => { console.log('Got a response from the API'); });
   }
